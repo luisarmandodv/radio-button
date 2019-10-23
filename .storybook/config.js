43dbfac95ce6b36301374ff2b7a6c90691edd782
Tomoko Yuki <tomoko.monzen@rakuten.com>
2019-10-24 08:29:52 +0900

    Added configuration setting to set stories inside component folder

--- .storybook/config.js
+++ .storybook/config.js
@@ -9,7 +9,8 @@ addParameters({
 });
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /\.jsx$/);
+const req = require.context('../src', true, /\.stories.jsx$/);
+
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
